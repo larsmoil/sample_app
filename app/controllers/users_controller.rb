8845a7ea1a9ea3342a9987a1203c889629f0aa67
Lars-Olof Moilanen <lars.olof.moilanen@gmail.com>
2012-02-03 00:53:57 +0100

    Finishes user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  respond_to :html, :json
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      respond_with(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    respond_with(@user = User.find(params[:id]))
   end
 end
